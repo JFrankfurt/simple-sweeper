dcbbe37df358ca6c5d5c3bffd540bc5d493b3df7
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-07-17 03:05:09 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,29 @@
 import { BigNumber } from '@ethersproject/bignumber'
 import { Deferrable } from '@ethersproject/properties'
 import { JsonRpcProvider, TransactionRequest } from '@ethersproject/providers'
+import { formatUnits } from '@ethersproject/units'
 import { Wallet } from '@ethersproject/wallet'
 import { config } from 'dotenv'
-import { utils } from 'ethers'
-import fetch from 'node-fetch'
+
 config({})
 
 enum Networks {
-  mainnet = 'mainnet',
-  rinkeby = 'rinkeby',
-  kovan = 'kovan',
-  ropsten = 'ropsten',
   goerli = 'goerli',
+  sepolia = 'sepolia',
+  base = 'base',
 }
 
 const ChainIds: Record<Networks, number> = {
-  mainnet: 1,
-  rinkeby: 4,
-  kovan: 42,
-  ropsten: 3,
   goerli: 5,
+  sepolia: 11155111,
+  base: 8453,
 }
 
 const SWEEP_FREQ = !!process.env.sweep_frequency ? parseInt(process.env.sweep_frequency) : 30 * 1000
 const WALLET_DEPTH = parseInt(process.env.sweep_depth) || 3
 
 function checkEnvironment(): void {
-  const requiredVars = [
-    'destination',
-    'sweep_mnemonic',
-    'sweep_depth',
-    'sweep_frequency',
-    'mainnet_rpc',
-    'rinkeby_rpc',
-    'kovan_rpc',
-    'ropsten_rpc',
-    'goerli_rpc',
-  ]
+  const requiredVars = ['destination', 'sweep_mnemonic', 'sweep_depth', 'sweep_frequency']
   const hasRequiredEnvVars = requiredVars.reduce((acc, cur) => (Boolean(process.env[cur]) ? acc : false), true)
 
   if (!hasRequiredEnvVars) {
@@ -45,10 +31,11 @@ function checkEnvironment(): void {
     throw new Error(`Missing required environment variables. Make your own .env file and include: ${missing}.`)
   }
 }
-async function getWallets(providers: Record<Networks, JsonRpcProvider>): Promise<Record<Networks, Wallet[]>> {
+async function getWallets(
+  providers: { [key in keyof typeof Networks]?: JsonRpcProvider }
+): Promise<Record<Networks, Wallet[]>> {
   const wallets = {}
-  const numNetworks = Object.values(Networks).length
-  for (let i = 0; i < numNetworks; i++) {
+  for (let i = 0; i < Object.values(Networks).length; i++) {
     const network = Object.values(Networks)[i]
     for (let j = 0; j < WALLET_DEPTH; j++) {
       const path = `m/44'/60'/0'/0/${j}`
@@ -65,53 +52,49 @@ async function getWallets(providers: Record<Networks, JsonRpcProvider>): Promise
   return wallets as Record<Networks, Wallet[]>
 }
 
-async function estimateGasPrice(provider: JsonRpcProvider): Promise<BigNumber | void> {
+async function estimateGasPrice(
+  provider: JsonRpcProvider
+): Promise<{
+  maxFeePerGas: BigNumber | undefined
+  maxPriorityFeePerGas: BigNumber | undefined
+}> {
+  let maxPriorityFeePerGas, maxFeePerGas
   try {
     const network = await provider.getNetwork()
-    if (network.name === 'homestead') {
-      try {
-        const response = await fetch('https://www.gasnow.org/api/v3/gas/price')
-        if (response.ok) {
-          const { data } = await response.json()
-          console.log(`gas price estimate for ${network.name}: ${utils.formatUnits(data.fast, 'gwei')}`)
-          return data.fast
-        }
-      } catch (error) {
-        console.error(`gasnow api call failed`, error)
-      }
-    }
+    // todo: cache prior blocks
     const block = await provider.getBlockWithTransactions('latest')
     const block1 = await provider.getBlockWithTransactions(-1)
-    const block2 = await provider.getBlockWithTransactions(-2)
-    const transactions = [...block.transactions, ...block1.transactions, ...block2.transactions]
+    const transactions = [...block.transactions, ...block1.transactions]
     const filteredTxList = transactions.filter((tx) => tx.gasPrice.gt(0)) // filter out miner stuff
     const gasPrices = filteredTxList.map((tx) => tx.gasPrice)
     const gasSum = gasPrices.reduce((acc, cur) => acc.add(cur), BigNumber.from(0))
     const divisor = gasPrices.length || 1
-    const average = gasSum.div(divisor).mul(102).div(100) // 2% gas price buffer over average rate
-    console.log(`gas price estimate for ${network.name}: ${utils.formatUnits(average, 'gwei')}`)
-    return average || BigNumber.from(1)
+    const average = gasSum.div(divisor).mul(105).div(100) // 5% gas price buffer over average rate
+    console.log(`gas price estimate for ${network.name}: ${formatUnits(average, 'gwei')}`)
   } catch (error) {
     console.error(`failed gas estimation: ${error}`)
-    return BigNumber.from(1)
   }
+  return { maxPriorityFeePerGas, maxFeePerGas }
 }
 
 async function main() {
   checkEnvironment()
-  const providers: Record<Networks, JsonRpcProvider> = {
-    [Networks.mainnet]: new JsonRpcProvider(process.env.mainnet_rpc, ChainIds[Networks.mainnet]),
-    [Networks.rinkeby]: new JsonRpcProvider(process.env.rinkeby_rpc, ChainIds[Networks.rinkeby]),
-    [Networks.kovan]: new JsonRpcProvider(process.env.kovan_rpc, ChainIds[Networks.kovan]),
-    [Networks.ropsten]: new JsonRpcProvider(process.env.ropsten_rpc, ChainIds[Networks.ropsten]),
+  const providers: { [key in keyof typeof Networks]?: JsonRpcProvider } = {
     [Networks.goerli]: new JsonRpcProvider(process.env.goerli_rpc, ChainIds[Networks.goerli]),
+    [Networks.sepolia]: new JsonRpcProvider(process.env.sepolia_rpc, ChainIds[Networks.sepolia]),
+    [Networks.base]: new JsonRpcProvider(process.env.base_rpc, ChainIds[Networks.base]),
   }
 
   const networkValues = Object.values(Networks)
   const gasPriceEstimates = {}
 
   networkValues.forEach((network) => {
-    estimateGasPrice(providers[network]).then((gasEstimate) => (gasPriceEstimates[network] = gasEstimate))
+    if (!providers[network]) {
+      return
+    }
+    estimateGasPrice(providers[network]).then(
+      ({ maxFeePerGas, maxPriorityFeePerGas }) => (gasPriceEstimates[network] = { maxFeePerGas, maxPriorityFeePerGas })
+    )
   })
 
   let wallets = await getWallets(providers)
@@ -125,6 +108,7 @@ async function main() {
         const balance = await wallet.getBalance()
         const transferCost = transferGasCost.mul(gasPriceEstimates[network])
         if (balance.gt(transferCost)) {
+          const { maxFeePerGas, maxPriorityFeePerGas } = gasPriceEstimates[network]
           console.log(`worth transacting on ${network}${i} as ${address}`)
           console.log(`balance: ${balance.toString()}`)
           console.log(`transferCost: ${transferCost.toString()}`)
@@ -132,7 +116,8 @@ async function main() {
             to: process.env.destination,
             from: wallet.address,
             gasLimit: transferGasCost,
-            gasPrice: gasPriceEstimates[network],
+            maxFeePerGas,
+            maxPriorityFeePerGas,
             value: balance.sub(transferCost),
             chainId: ChainIds[network],
           }
@@ -144,29 +129,15 @@ async function main() {
       console.error('error sweeping', error)
     }
   }
-  async function handleBlock(network: Networks, blocknumber: number): Promise<any> {
-    if (!gasPriceEstimates[network]) {
-      return
-    }
-    if (blocknumber % 4 !== 0) {
-      try {
-        estimateGasPrice(providers[network]).then((price) => (gasPriceEstimates[network] = price))
-      } catch (error) {
-        console.error('error setting gas estimate', error)
-      }
-    }
-    return sweep(network)
-  }
 
   return new Promise<void>(() => {
-    providers[Networks.mainnet].on('block', async (blockNumber) => await handleBlock(Networks.mainnet, blockNumber))
-    const testnets = networkValues.filter((network) => network !== Networks.mainnet)
+    // providers[Networks.base].on('block', async (blockNumber) => await handleBlock(Networks.base, blockNumber))
     setInterval(async () => {
-      const sweeps = testnets.map(sweep)
+      const sweeps = networkValues.map(sweep)
       await Promise.all(sweeps)
     }, SWEEP_FREQ)
     setInterval(() => {
-      testnets.forEach((network) => {
+      networkValues.forEach((network) => {
         estimateGasPrice(providers[network]).then((price) => (gasPriceEstimates[network] = price))
       })
     }, SWEEP_FREQ ^ 1.06)
